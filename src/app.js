93edec9b4f3e993baaa858859d6d6daf3de0a8db
Cristian Trapiello <119610379+CrisMader@users.noreply.github.com>
2025-11-11 20:18:55 +0000

    Second Try

--- src/app.js
+++ src/app.js
@@ -7,10 +7,15 @@ const palos = ['♦', '♥', '♠', '♣'];
 const num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
 let contador = 0;
+let finalResult = 0;
 
 const container = document.createElement('div')
-container.id = 'container'
+container.id = 'container_1'
 document.body.appendChild(container);
+
+const cpuContainer = document.createElement('div')
+cpuContainer.id = 'container_2'
+document.body.append(cpuContainer);
 
 const btnCardFirst = document.createElement('button')
 btnCardFirst.innerText = 'Generate Random Card'
@@ -21,6 +26,11 @@ const btnCardTwo = document.createElement('button')
 btnCardTwo.innerText = 'Generar Otra Carta Aleatoria'
 btnCardTwo.classList.add('buttonGenerator')
 document.body.appendChild(btnCardTwo)
+
+const btnCPU = document.createElement('button');
+btnCPU.innerText = 'Valor de Cartas CPU';
+btnCPU.classList.add('buttonGenerator');
+document.body.appendChild(btnCPU);
 
 const randomNumber = (arr) => {
   return arr[Math.floor(Math.random() * arr.length)];
@@ -38,8 +48,11 @@ const createCard = () => {
   pal1.classList.add('palOfCardTop')
   pal2.classList.add('palOfCardBottom')
 
-  number.innerHTML = `<span>${randomNumber(num)}</span>`
-  pal1.innerHTML = `<span>${randomNumber(palos)}</span>`
+  const valor = randomNumber(num);
+  const paloss = randomNumber(palos);
+
+  number.innerHTML = `<span>${num}</span>`
+  pal1.innerHTML = `<span>${paloss}</span>`
   pal2.innerHTML = pal1.innerHTML;
 
   pal1.className = 'palOfCardTop';
@@ -60,23 +73,36 @@ const createCard = () => {
   card.appendChild(pal2);
   
   console.log(`El numero es ${number.textContent}, y el palo es ${pal1.textContent}`)
-
+  card.dataset.valor = valor;
   return card;
 }
 
 btnCardFirst.addEventListener('click', () => {
   container.innerHTML = '';
+  contador = 1;
+  finalResult = 
   container.appendChild(createCard());
+  //console.log(contador);
 })
 
 btnCardTwo.addEventListener('click', () =>{
-  contador++
-  if(contador < 1){
+  if(contador < 4){
     container.appendChild(createCard());
+    contador++;
+    finalResult += parseInt(card.dataset.valor);
+    console.log('El valor actual del jugador principal es --> ' + finalResult);
+  }else{
+    alert('⚠️ Solo puedes tener 4 cartas.')
   }
-  if(contador >= 1){
-    container.appendChild(createCard());
+  //console.log(contador);
+})
+
+btnCPU.addEventListener('click', () =>{
+  let aux = Math.floor(Math.random() * 22);
+  while(aux < 13){
+    aux = Math.floor(Math.random() * 22);
   }
+  //console.log(aux);
 })
 
 window.onload = function () {
